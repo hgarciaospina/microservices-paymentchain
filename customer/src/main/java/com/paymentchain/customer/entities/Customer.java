5d09fb3ff5c919d47a97d825c97dda4713d97982
Henry García <henrygarciaospina@gmail.com>
2024-08-06 19:00:37 -0500

    Correction of POM dependencies and adjustments to Customer entity property validations.

--- customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -16,10 +18,13 @@ public class Customer {
     private long id;
 
     @NotEmpty(message = "Name is required")
-   @Pattern(regexp = "^([A-Z][a-z]+)( [A-Z][a-z]+)*$", message = "Name must be in proper case with only letters and single spaces between names")
-    
+    @NotNull(message = "Name is required")
+    @Pattern(regexp = "^([A-Z][a-z]+)( [A-Z][a-z]+)*$", message = "Name must be in proper case with only letters and single spaces between names")
+    @Size(min = 6, max = 150, message = "Name must be between 6 and 150 characters")
     private String name;
 
+    @NotEmpty(message = "Phone is required")
+    @NotNull(message = "Phone is required")
     @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Phone number is invalid")
     private String phone;
 }
